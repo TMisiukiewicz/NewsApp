661f4971379ad3925f3f546b94a742ba28b3fb1e
Tomasz Misiukiewicz <tomasz.misiukiewicz@callstack.com>
2023-01-09 12:06:45 +0100

    add nav setup

--- App.tsx
+++ App.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
+import {SafeAreaView} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
-import {SafeAreaView, View} from 'react-native';
+import MainStack from './src/navigation/MainStack';
 
 const client = new QueryClient();
 
 const App = () => {
   return (
     <QueryClientProvider {...{client}}>
-      <SafeAreaView>
-        <View />
+      <SafeAreaView style={{flex: 1}}>
+        <NavigationContainer>
+          <MainStack />
+        </NavigationContainer>
       </SafeAreaView>
     </QueryClientProvider>
   );
